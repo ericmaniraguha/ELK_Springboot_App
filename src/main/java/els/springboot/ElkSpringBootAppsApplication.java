f90c59b2a810e5290027410d5879484e9557f17d
ericmaniraguha <ericmaniraguha@gmail.com>
2023-07-19 21:34:56 +0200

    done by creating elk with controller, service and entities

---
+++ src/main/java/els/springboot/ElkSpringBootAppsApplication.java
@@ -0,0 +1,13 @@
+package els.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkSpringBootAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElkSpringBootAppsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/els/springboot/UserController.java
@@ -0,0 +1,23 @@
+package els.springboot;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/getUser/{id}")
+    public User getUserById(@PathVariable int id) {
+        return userService.getUserById(id);
+    }
+}
